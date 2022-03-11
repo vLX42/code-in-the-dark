46699653cb24a114a9e333e403bc4283bd5ba71f
Peter Biro <github@vlx.dk>
2022-03-11 10:32:50 +0100

    Thanks page added

--- lib/persist.ts
+++ lib/persist.ts
@@ -1,7 +1,7 @@
 import { configurePersist } from 'zustand-persist'
 
 const { persist, purge } = configurePersist({
-  storage: typeof localStorage !== 'undefined' ? localStorage : null,
+  storage: typeof localStorage !== 'undefined' ? localStorage : null as any,
 })
 export default persist
 export { purge }
---
+++ pages/thanks.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useRef, useEffect } from "react";
+import { useEntryStore } from "../hooks/useEntryStore";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+  const { entry, updateSubmitted } = useEntryStore();
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    const doc = iframeRef?.current?.contentDocument;
+    doc?.open();
+    doc?.write(entry?.html || "");
+    doc?.close();
+  }, []);
+
+  return (
+    <center>
+      <h1>Thanks!</h1>
+      <h3>Your entry has been submitted</h3>
+      <iframe ref={iframeRef} />
+      <div
+        onClick={() => {
+          updateSubmitted(false);
+          router.push("/editor");
+        }}
+      >
+        Secret back button
+      </div>
+    </center>
+  );
+};
+
+export default Home;
